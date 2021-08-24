56412f402df4d7ef2d0086928c1c3d77d98481ee
andreyuniar <58914618+andreyuniar@users.noreply.github.com>
2021-08-25 00:13:42 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Excalibur Kernel | Lavender
+kernel.string=Excalibur Surya OSS
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
+device.name1=surya
 supported.versions=9 - 12
 supported.patchlevels=
 '; } # end properties
