8e6dc918a58f4290a01d34937845b4e4baab6cfb
andreyuniar <58914618+andreyuniar@users.noreply.github.com>
2021-08-31 17:01:12 +0700

    Ak3

--- anykernel.sh
+++ anykernel.sh
@@ -4,12 +4,17 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Excalibur Surya OSS
+kernel.string=Excalibur Kernel
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=surya
+device.name2=karna
+device.name3=
+device.name4=
+device.name5=
 supported.versions=10-11
 supported.patchlevels=
 '; } # end properties
@@ -24,11 +29,30 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## AnyKernel install
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel boot install
 dump_boot;
-
-# end ramdisk changes
-
 write_boot;
-## end install
+## end boot install
 
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
